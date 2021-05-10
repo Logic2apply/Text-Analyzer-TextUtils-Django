c5d79602279e62a51432f00f8d85ffc692f6392f
Abhishek Kapoor <kapoorkk1980@gmail.com>
2021-05-10 11:58:03 +0530

    Added views.py and layed pipeline

---
+++ TextUtils/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Home")
+
+def removepunc(request):
+    return HttpResponse("remove punc")
+
+def capfirst(request):
+    return HttpResponse("capitalize first")
+
+def newlineremove(request):
+    return HttpResponse("capitalize first")
+
+def spaceremove(request):
+    return HttpResponse("space remover")
+
+def charcount(request):
+    return HttpResponse("charcount ")
